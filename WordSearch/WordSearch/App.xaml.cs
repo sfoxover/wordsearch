47f2600e334eb66bbce66058b9568dd0f6bbe89d
sfoxover <sfoxover@gmail.com>
2018-04-16 09:07:03 -0700

    Add project files.

---
+++ WordSearch/WordSearch/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace WordSearch
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new WordSearch.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
