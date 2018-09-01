610c2db55f5f7d5ef9714c3d67a0ee9e8090fce3
sfoxover <sfoxover@gmail.com>
2018-09-01 16:15:35 -0700

    - check in log file window

---
+++ WordSearch/WordSearch.UWP/html/js/logFiles.js
@@ -0,0 +1,178 @@
+﻿// on ready handler
+$(document).ready(function () {    
+    logFiles.signalNativeApp('ping');
+
+    // close window
+    $('#closeButton').click(() => logFiles.closeWindow());
+
+    // clear logs
+    $('#clearLogs').click(() => logFiles.clearLogs());
+});
+
+class LogFiles {
+    constructor() {
+        this.waitForCallbackCreation().then(function () {
+            console.log('Native object loaded');
+        }).catch(function (error) {
+            console.log('Loading mock data');
+        });
+    }   
+
+    // use promise to wait for C# logsJSCallback object to be created
+    waitForCallbackCreation() {
+        try {
+            return new Promise(function (resolve, reject) {
+                var timerId = setTimeout(function () {
+                    return reject(new Error("waitForCallbackCreation timed out."));
+                }, 3000);
+                (function waitForCallback() {
+                    try {
+                        if (logsJSCallback) {
+                            clearTimeout(timerId);
+                            return resolve();
+                        }
+                    }
+                    catch (err) {
+                        err;
+                    }
+                    setTimeout(waitForCallback, 100);
+                })();
+            });
+        }
+        catch (err) {
+            this.handleError(err);
+        }
+    }
+
+    // handle errors
+    handleError(err) {
+        try {
+            this.waitForCallbackCreation().then(function () {
+                // format error message into json
+                var msgObj = new Object();
+                msgObj.Message = "Error";
+                msgObj.Data = err;
+                var json = JSON.stringify(msgObj);
+                // call native code
+                logsJSCallback(json);
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    // log message to native app
+    logMsg(info) {
+        try {
+            this.waitForCallbackCreation().then(function () {
+                // format error message into json
+                var msgObj = new Object();
+                msgObj.Message = "LogMsg";
+                msgObj.Data = info;
+                var json = JSON.stringify(msgObj);
+                // call native code
+                logsJSCallback(json);
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }    
+
+    // create word list table
+    makeTable(container, items) {
+        try {
+            // create table elements
+            var table = $('#logFilesTable');   
+            var body = $('<tbody/>');
+            var rank = 1;
+            $.each(items, function (rowCount, item) {
+                var row = $("<tr/>");
+                var log = item.Log;
+                var date = item.CreatedDate;
+                row.append($("<td/>").text(log).addClass('text-center'));
+                row.append($("<td/>").text(date).addClass('text-center'));
+                body.append(row);
+                rank++;
+            });
+            $("#logFilesTable tbody").remove();
+            table.append(body);
+        }
+        catch(err) {
+            this.handleError(err);
+        }
+    }   
+
+    // pass message and data to native app
+    signalNativeApp(msg, data) {
+        try {
+            this.waitForCallbackCreation().then(function () {
+                // format message into json
+                var msgObj = new Object();
+                msgObj.Message = msg;
+                msgObj.Data = data;
+                var json = JSON.stringify(msgObj);
+                // call native code
+                logsJSCallback(json);
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
+        catch (err) {
+            this.handleError(err);
+        }
+    }
+
+    // handle message from native app
+    handleMsgFromApp(json) {
+        try {
+            var msgObj = JSON.parse(json);
+            var msg = msgObj.Message;
+            var data = msgObj.Data;
+            switch (msg) {
+                case "LoadLogs":
+                    {
+                        this.makeTable($("#logFiles"), data);
+                        break;
+                    }
+                default:
+                    {
+                        this.handleError("Unknown message from app, " + msg);
+                        break;
+                    }
+            }
+        }
+        catch (err) {
+            this.handleError(err);
+        }
+    }
+
+    // close window
+    closeWindow() {
+        try {
+            logFiles.signalNativeApp('closeWindow', name);
+        }
+        catch (err) {
+            this.handleError(err);
+        }
+    }
+
+    // clear logs
+    clearLogs() {
+        try {
+            logFiles.signalNativeApp('clearLogs');
+        }
+        catch (err) {
+            this.handleError(err);
+        }
+    }
+}
+
+// global object
+var logFiles = new LogFiles();
+
---
+++ WordSearch/WordSearch/ViewModels/LogFilesPageViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using WordSearch.Helpers;
+using WordSearch.Models;
+using Xam.Plugin.WebView.Abstractions;
+using Xamarin.Forms;
+
+namespace WordSearch.ViewModels
+{
+    public class LogFilesPageViewModel : BindableBase
+    {
+        // reference to web page
+        private FormsWebView WebView { get; set; }
+        private string _sourceHtml;
+        public string SourceHtml
+        {
+            get { return _sourceHtml; }
+            set { SetProperty(ref _sourceHtml, value); }
+        }
+        // local html page width
+        private double _htmlPageWidth;
+        public double HtmlPageWidth
+        {
+            get { return _htmlPageWidth; }
+            set { SetProperty(ref _htmlPageWidth, value); }
+        }
+        // local header html page height
+        private double _htmlPageHeight;
+        public double HtmlPageHeight
+        {
+            get { return _htmlPageHeight; }
+            set { SetProperty(ref _htmlPageHeight, value); }
+        }
+        // has the html page loaded
+        public bool HasHtmlPageSignalled { get; set; }
+
+        public LogFilesPageViewModel(INavigation navigation, FormsWebView webView)
+            : base(navigation)
+        {
+            HasHtmlPageSignalled = false;
+            WebView = webView;
+            SourceHtml = "html/logFiles.html";
+        }
+
+        // send message to html header page
+        public async Task<bool> SignalHtmlPage(string message, object data)
+        {
+            bool bOK = true;
+            try
+            {
+                // wait for page load
+                int waited = 0;
+                while (!HasHtmlPageSignalled && waited < 20)
+                {
+                    await Task.Delay(100);
+                    waited++;
+                }
+                Debug.Assert(HasHtmlPageSignalled);
+                var msg = new MessageJson();
+                msg.Message = message;
+                msg.Data = data;
+                string json = msg.GetJsonString();
+                string script = $"logFiles.handleMsgFromApp('{json}')";
+                await WebView.InjectJavascriptAsync(script).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.Error($"SignalHtmlPage exception, {ex.Message}");
+                bOK = false;
+            }
+            return bOK;
+        }
+
+        // clear logs from database
+        internal async void ClearLogs()
+        {
+            bool bOK = await Logger.Instance.DeleteAllRecords();
+            if (bOK)
+            {
+                await SignalHtmlPage("LoadLogs", null);
+            }
+        }
+
+        // load logs from database
+        public async void LoadData()
+        {
+            bool bOK = Logger.Instance.LoadAllRecords(out List<DebugLog> results);
+            if (bOK)
+            {
+                await SignalHtmlPage("LoadLogs", results);
+            }
+        }
+    }
+}
